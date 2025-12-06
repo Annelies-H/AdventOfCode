33c9616e8b750d0351a2f41f28be977542b446eb
Annelies van Hoesel <avanhoesel@gmail.com>
2025-12-06 17:11:49 +0100

    move 2023 code to its own folder

---
+++ 2023_python/main.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import day19
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #print("Day 1 part 1: " + str(day01.part_one()))
+    #print("Day 1 part 2 test: " + str(day01.part_two("./inputs/day01_test2.txt")))
+    #print("Day 1 part 2: " + str(day01.part_two()))
+    #print("Day 2 part 1 test: " + str(day02.part_one("./inputs/day02_test.txt")))
+    #print("Day 2 part 1: " + str(day02.part_one()))
+    #print("Day 2 part 2 test: " + str(day02.part_two("./inputs/day02_test.txt")))
+    #print("Day 2 part 2: " + str(day02.part_two()))
+
+    #print("day 4 part 1 test: " + str(day04.part_one("./inputs/day04_test.txt")))
+    #print("Day 4 part 1: " + str(day04.part_one()))
+    #print("Day 4 part 2 test: " + str(day04.part_two("./inputs/day04_test.txt")))
+    #print("Day 4 part 2: " + str(day04.part_two()))
+
+    # Day 6 part 1 took a little excel detour for funsies
+    #print("day 6 part 2: " + str(day06.part_two()))
+    #print("day 7 part 1 test: " + str(day07.part_one("./inputs/day07_test.txt")))
+    #print("day 7 part 1: " + str(day07.part_one()))
+    #print("day 7 part 2 test: " + str(day07.part_two("./inputs/day07_test.txt")))
+    #print("day 7 part 2: " + str(day07.part_two()))
+    #print("day 8 part 2 test: " + str(day08.part_two("./inputs/day08_test2.txt")))
+    #print("day 8 part 2 test: " + str(day08.part_two()))
+
+    #print("day 11 part 1 test: " + str(day11.part_one("./inputs/day11_test.txt")))
+    #print("day 11: " + str(day11.part_one()))
+
+    #print("day 19 part 1 test: " + str(day19.part_one("./inputs/day19_test.txt")))
+    print("day 19 part 1: " + str(day19.part_one()))
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
